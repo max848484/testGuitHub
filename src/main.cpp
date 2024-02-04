adfaf273bdccadccd8b67adbb4783207a9660855
Maxime <mchampagne@cmaisonneuve.qc.ca>
2024-02-03 21:39:41 -0500

    GitHub test

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+#include "board_mapping.h"
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+
+  
+  pinMode(GPIO_DATA_LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(GPIO_DATA_LED, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(GPIO_DATA_LED, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+  
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
